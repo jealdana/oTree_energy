9575e742967553fff06c5ed5fe0f3f405a0c950d
Aldana-Sigona, Jesus-Enrique <jealdana@users.noreply.github.com>
2017-12-26 01:53:45 -0500

    heroku error 001

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #!/usr/bin/env python
 import os
 import sys
@@ -10,27 +8,3 @@ if __name__ == "__main__":
 
     from otree.management.cli import execute_from_command_line
     execute_from_command_line(sys.argv, script_file=__file__)
-=======
-#!/usr/bin/env python
-import os
-import sys
-
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-    from otree.management.cli import execute_from_command_line
-    execute_from_command_line(sys.argv, script_file=__file__)
->>>>>>> refs/remotes/origin/master
-=======
-#!/usr/bin/env python
-import os
-import sys
-
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-    from otree.management.cli import execute_from_command_line
-    execute_from_command_line(sys.argv, script_file=__file__)
->>>>>>> refs/remotes/origin/master
