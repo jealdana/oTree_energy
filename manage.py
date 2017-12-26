19649e100c2a92241eb51914f98c22ea0cb88fa1
Aldana-Sigona, Jesus-Enrique <jealdana@users.noreply.github.com>
2017-12-26 01:18:04 -0500

    Initial commit

---
+++ manage.py
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+#!/usr/bin/env python
+import os
+import sys
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from otree.management.cli import execute_from_command_line
+    execute_from_command_line(sys.argv, script_file=__file__)
+=======
+#!/usr/bin/env python
+import os
+import sys
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from otree.management.cli import execute_from_command_line
+    execute_from_command_line(sys.argv, script_file=__file__)
+>>>>>>> refs/remotes/origin/master
+=======
+#!/usr/bin/env python
+import os
+import sys
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from otree.management.cli import execute_from_command_line
+    execute_from_command_line(sys.argv, script_file=__file__)
+>>>>>>> refs/remotes/origin/master
